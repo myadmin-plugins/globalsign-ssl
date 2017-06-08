f3a232acff14834f93210e21723fdab64b017aa6
Joe Huss <detain@interserver.net>
2017-06-08 00:59:17 -0400

    plugin definition updates

--- myadmin.php
+++ myadmin.php
@@ -8,17 +8,17 @@ return [
 	'name' => 'Globalsign Ssl',
 	'description' => 'Allows selling of Globalsign Server and VPS License Types.  More info at https://www.netenberg.com/globalsign.php',
 	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a globalsign license. Allow 10 minutes for activation.',
-	'module' => 'licenses',
+	'module' => 'ssl',
 	'author' => 'detain@interserver.net',
 	'home' => 'https://github.com/detain/myadmin-globalsign-ssl',
 	'repo' => 'https://github.com/detain/myadmin-globalsign-ssl',
 	'version' => '1.0.0',
-	'type' => 'licenses',
+	'type' => 'service',
 	'hooks' => [
 		/*'function.requirements' => ['Detain\MyAdminGlobalsign\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminGlobalsign\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminGlobalsign\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminGlobalsign\Plugin', 'ChangeIp'],
+		'ssl.settings' => ['Detain\MyAdminGlobalsign\Plugin', 'Settings'],
+		'ssl.activate' => ['Detain\MyAdminGlobalsign\Plugin', 'Activate'],
+		'ssl.change_ip' => ['Detain\MyAdminGlobalsign\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminGlobalsign\Plugin', 'Menu'] */
 	],
 ];
