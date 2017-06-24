91f42fe8b6e1ca8f2d02357cfb0101b7f88a0edf
Joe Huss <detain@interserver.net>
2017-06-24 15:09:34 -0400

    a bunch of updates improving the code making use of self:: and other imprvoements

--- src/Plugin.php
+++ src/Plugin.php
@@ -26,7 +26,7 @@ class Plugin {
 	public static function getActivate(GenericEvent $event) {
 		$license = $event->getSubject();
 		if ($event['category'] == SERVICE_TYPES_FANTASTICO) {
-			myadmin_log('licenses', 'info', 'Globalsign Activation', __LINE__, __FILE__);
+			myadmin_log(self::$module, 'info', 'Globalsign Activation', __LINE__, __FILE__);
 			function_requirements('activate_globalsign');
 			activate_globalsign($license->get_ip(), $event['field1']);
 			$event->stopPropagation();
@@ -35,11 +35,10 @@ class Plugin {
 
 	public static function getMenu(GenericEvent $event) {
 		$menu = $event->getSubject();
-		$module = 'licenses';
 		if ($GLOBALS['tf']->ima == 'admin') {
-			$menu->add_link($module, 'choice=none.reusable_globalsign', 'icons/database_warning_48.png', 'ReUsable Globalsign Licenses');
-			$menu->add_link($module, 'choice=none.globalsign_list', 'icons/database_warning_48.png', 'Globalsign Licenses Breakdown');
-			$menu->add_link($module.'api', 'choice=none.globalsign_licenses_list', 'whm/createacct.gif', 'List all Globalsign Licenses');
+			$menu->add_link(self::$module, 'choice=none.reusable_globalsign', 'icons/database_warning_48.png', 'ReUsable Globalsign Licenses');
+			$menu->add_link(self::$module, 'choice=none.globalsign_list', 'icons/database_warning_48.png', 'Globalsign Licenses Breakdown');
+			$menu->add_link(self::$module.'api', 'choice=none.globalsign_licenses_list', 'whm/createacct.gif', 'List all Globalsign Licenses');
 		}
 	}
 
@@ -60,14 +59,13 @@ class Plugin {
 	}
 
 	public static function getSettings(GenericEvent $event) {
-		$module = 'ssl';
 		$settings = $event->getSubject();
-		$settings->add_text_setting($module, 'API Settings', 'globalsign_username', 'GlobalSign Username:', 'Username to use for GlobalSign API Authentication', $settings->get_setting('GLOBALSIGN_USERNAME'));
-		$settings->add_text_setting($module, 'API Settings', 'globalsign_password', 'GlobalSign Password:', 'Password to use for GlobalSign API Authentication', $settings->get_setting('GLOBALSIGN_PASSWORD'));
-		$settings->add_text_setting($module, 'API Settings', 'globalsign_test_username', 'GlobalSign Username:', 'Username to use for GlobalSign API Testing Authentication', $settings->get_setting('GLOBALSIGN_TEST_USERNAME'));
-		$settings->add_text_setting($module, 'API Settings', 'globalsign_test_password', 'GlobalSign Password:', 'Password to use for GlobalSign API Testing Authentication', $settings->get_setting('GLOBALSIGN_TEST_PASSWORD'));
-		$settings->add_dropdown_setting($module, 'Out of Stock', 'globalsign_testing', 'GlobalSign Test Mode', 'Enable API Test mode (doesnt create real certs or cost)', GLOBALSIGN_TESTING, array('false', 'true'), array('No', 'Yes', ));
-		$settings->add_dropdown_setting($module, 'Out of Stock', 'outofstock_globalsign_ssl', 'Out Of Stock GlobalSign SSL', 'Enable/Disable Sales Of This Type', OUTOFSTOCK_GLOBALSIGN_SSL, array('0', '1'), array('No', 'Yes', ));
+		$settings->add_text_setting(self::$module, 'API Settings', 'globalsign_username', 'GlobalSign Username:', 'Username to use for GlobalSign API Authentication', $settings->get_setting('GLOBALSIGN_USERNAME'));
+		$settings->add_text_setting(self::$module, 'API Settings', 'globalsign_password', 'GlobalSign Password:', 'Password to use for GlobalSign API Authentication', $settings->get_setting('GLOBALSIGN_PASSWORD'));
+		$settings->add_text_setting(self::$module, 'API Settings', 'globalsign_test_username', 'GlobalSign Username:', 'Username to use for GlobalSign API Testing Authentication', $settings->get_setting('GLOBALSIGN_TEST_USERNAME'));
+		$settings->add_text_setting(self::$module, 'API Settings', 'globalsign_test_password', 'GlobalSign Password:', 'Password to use for GlobalSign API Testing Authentication', $settings->get_setting('GLOBALSIGN_TEST_PASSWORD'));
+		$settings->add_dropdown_setting(self::$module, 'Out of Stock', 'globalsign_testing', 'GlobalSign Test Mode', 'Enable API Test mode (doesnt create real certs or cost)', GLOBALSIGN_TESTING, array('false', 'true'), array('No', 'Yes', ));
+		$settings->add_dropdown_setting(self::$module, 'Out of Stock', 'outofstock_globalsign_ssl', 'Out Of Stock GlobalSign SSL', 'Enable/Disable Sales Of This Type', OUTOFSTOCK_GLOBALSIGN_SSL, array('0', '1'), array('No', 'Yes', ));
 	}
 
 }
