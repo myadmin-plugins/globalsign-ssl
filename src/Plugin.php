32d3b58f45505b909e9ade964a610fe596e87a3e
Joe Huss <detain@interserver.net>
2017-06-24 06:12:05 -0400

    removing changeip call

--- src/Plugin.php
+++ src/Plugin.php
@@ -34,27 +34,6 @@ class Plugin {
 		}
 	}
 
-	public static function ChangeIp(GenericEvent $event) {
-		if ($event['category'] == SERVICE_TYPES_FANTASTICO) {
-			$license = $event->getSubject();
-			$settings = get_module_settings('licenses');
-			$globalsign = new Globalsign(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
-			myadmin_log('licenses', 'info', "IP Change - (OLD:".$license->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
-			$result = $globalsign->editIp($license->get_ip(), $event['newip']);
-			if (isset($result['faultcode'])) {
-				myadmin_log('licenses', 'error', 'Globalsign editIp('.$license->get_ip().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
-				$event['status'] = 'error';
-				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
-			} else {
-				$GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $license->get_ip());
-				$license->set_ip($event['newip'])->save();
-				$event['status'] = 'ok';
-				$event['status_text'] = 'The IP Address has been changed.';
-			}
-			$event->stopPropagation();
-		}
-	}
-
 	public static function Menu(GenericEvent $event) {
 		// will be executed when the licenses.settings event is dispatched
 		$menu = $event->getSubject();
